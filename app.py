79d96a67a7e1b1fa05b5d28e8c35cb56fdd94d90
Famous Samos <spearlman2@gmail.com>
2022-08-12 21:56:07 -0400

    finished the baseline code for the project....working MVP

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
@@ -14,26 +15,19 @@ w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 # The Load_Contract Function
 ################################################################################
 
+concert_database = {
+    "General Admission": ["General Admission", "SMART_CONTRACT_ADDRESS_GA", 50, './contracts/compiled/general_admission.json', "https://raw.githubusercontent.com/majikthise911/Access_Token/devFork-JC/Screenshot%202022-08-11%20213953.png"],
+    "Pit": ["Pit", "SMART_CONTRACT_ADDRESS_PIT", 150, './contracts/compiled/pit.json', "https://raw.githubusercontent.com/majikthise911/Access_Token/devFork-JC/Screenshot%202022-08-11%20213742.png"],
+    "Back Stage": ["Back Stage", "SMART_CONTRACT_ADDRESS_BS", 300, './contracts/compiled/back_stage.json', "https://raw.githubusercontent.com/majikthise911/Access_Token/devFork-JC/Screenshot%202022-08-11%20214135.png"]
+}
+
 
 @st.cache(allow_output_mutation=True)
 def load_contract():
-
-    # Load the contract ABI
-    if tiers == "Back Stage":
-        j_path = './contracts/compiled/artwork_abi1.json'
-        smart_contract_address = "0xA595B3684a73aCA80E16Aa499B419AE0f96A8293"
-    elif tiers == "Pit":
-        j_path = './contracts/compiled/artwork_abi2.json'
-        smart_contract_address = "0xA595B3684a73aCA80E16Aa499B419AE0f96A8293"
-    else:
-        j_path = './contracts/compiled/artwork_abi3.json'
-        smart_contract_address = "0xA595B3684a73aCA80E16Aa499B419AE0f96A8293"
-
-
-        with open(Path(j_path)) as f:
-            artwork_abi = json.load(f)
-
-    contract_address = os.getenv(smart_contract_address)
+   
+    with open(Path(concert_database[ticket_tier][3])) as f:
+        artwork_abi = json.load(f)
+    contract_address = os.getenv(concert_database[ticket_tier][1])
 
     # Load the contract
     contract = w3.eth.contract(
@@ -43,11 +37,9 @@ def load_contract():
 
     return contract
 
-contract = load_contract()
-
 
 ################################################################################
-# Register New Artwork
+# Register New Ticket
 ################################################################################
 st.title("Enter Your Festival Experience")
 accounts = w3.eth.accounts
@@ -61,26 +53,32 @@ address = st.selectbox("Select Ticket Owner", options=accounts)
 st.markdown("# Choose Your Pass")
 ticket_tier = st.selectbox("Select Your Ticket Option", options=tiers)
 
-# Use a Streamlit component to get the artwork's URI
-if tiers == "Back Stage":
-    artwork_uri = "Artwork URI 1"
-elif tiers == "Pit":
-    artwork_uri = "Artwork URI 2"
-else:
-    artwork_uri = "Artwork URI 3"
+# Register New Ticket
+################################################################################
+ETH_data = requests.get("https://api.alternative.me/v2/ticker/Ethereum/?convert=USD").json()
+ether_exchange_rate = ETH_data['data']['1027']['quotes']['USD']['price']
+ticket_cost_ether = round(concert_database[ticket_tier][2] / ether_exchange_rate, 3)
+ticket_cost_wei = w3.toWei(ticket_cost_ether, 'ether')
+st.write(f"Ticket cost is ${concert_database[ticket_tier][2]} = {ticket_cost_ether} ETH")
+contract = load_contract()
+
+# Get Artwork URI from initial dictionary up top
+artwork_uri = concert_database[ticket_tier][4]
 
 
 if st.button("Purchase Ticket"):
 
-    # Use the contract to send a transaction to the registerArtwork function
-    
+    # Use the contract to send a transaction to the registerArtwork function and send a transaction that transfers the cost of the ticket to the company
+    # if contract.functions.totalSupply().call <500:
     tx_hash = contract.functions.registerArtwork(
         address,
         artwork_uri
     ).transact({'from': address, 'gas': 1000000})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    w3.eth.send_transaction({'to': '0x1Cde67bB7Dc95153EC27e833eB6Be0BfED471C86', 'from': address , 'gas': 1000000, 'value': ticket_cost_wei})
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
+    # Else:  
 
 st.markdown("---")
 
@@ -100,14 +98,14 @@ st.markdown("## Check  Ownership and Display Token")
 
 total_token_supply = contract.functions.totalSupply().call()
 
-token_id = st.selectbox("Artwork Tokens", list(range(total_token_supply)))
+token_id = st.selectbox("Ticket Tokens", list(range(total_token_supply)))
 
 if st.button("Display"):
 
     # Get the art token owner
     owner = contract.functions.ownerOf(token_id).call()
     
-    st.write(f"The token is registered to {owner}")
+    st.write(f"The ticket is registered to {owner}")
 
     # Get the art token's URI
     token_uri = contract.functions.tokenURI(token_id).call()
